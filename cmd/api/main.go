da2af3ed7246eed640e093949686bf128a7e0009
prLorence <paulorencereyes@gmail.com>
2024-07-21 17:56:05 +0800

    feat: setup gofiber and routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
 
-// what i will need
-// sqlc
-// fiber
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/prLorence/books-api/config"
+	"github.com/prLorence/books-api/internal/db"
+)
+
 // middleware, for authorization
 // decoder from request to application struct
 // db connection string comes from os.env, which come from config struct, might need an if statement for dev purposes
 // session manager
 // a seeder
 
+type Application struct {
+}
+
 func main() {
-	fmt.Println("hello world")
+	app := fiber.New()
+	app.Use(cors.New())
+
+	config := config.NewConfig()
+	conn, err := db.ConnectDB(config.DB_CONN)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v\n", err)
+	}
+
+	db := db.New(conn)
+
+	Routes(app, db)
+
+	fmt.Fprintf(os.Stdout, "Listening on host port %s", config.HOST_PORT)
+	err = app.Listen(fmt.Sprintf(":%s", config.APP_PORT))
+	log.Fatal(err)
 }
