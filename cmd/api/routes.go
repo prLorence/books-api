0435fd9156303f9e9b493bf1cb7c304ac00b7f39
prLorence <paulorencereyes@gmail.com>
2024-07-21 19:10:25 +0800

    fix(refactor): move author routes to its own package

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/prLorence/books-api/internal/authors"
 	"github.com/prLorence/books-api/internal/db"
 )
 
-func Routes(app *fiber.App, db *db.Queries) http.Handler {
-	app.Post("/authors", func(c *fiber.Ctx) error {
-		c.Accepts("application/json")
-		return authors.CreateAuthor(c, db)
-	})
-
-	app.Get("/authors", func(c *fiber.Ctx) error {
-		return authors.GetAuthor(c, db)
-	})
-	return nil
+func Routes(app *fiber.App, db *db.Queries) {
+	authors.SetupRoutes(app, db)
 }
