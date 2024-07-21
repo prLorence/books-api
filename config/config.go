fe043f08b4af085902fac41271c258778ac8cd2c
prLorence <paulorencereyes@gmail.com>
2024-07-21 18:02:49 +0800

    feat: additional config fields

--- config/config.go
+++ config/config.go
@@ -3,11 +3,15 @@ package config
 import "os"
 
 type Config struct {
-	DB_CONN string
+	DB_CONN   string
+	APP_PORT  string
+	HOST_PORT string
 }
 
 func NewConfig() *Config {
 	return &Config{
-		DB_CONN: os.Getenv("DB_CONN"),
+		DB_CONN:   os.Getenv("DB_CONN"),
+		APP_PORT:  os.Getenv("APP_PORT"),
+		HOST_PORT: os.Getenv("HOST_PORT"),
 	}
 }
