d0d29ea623de782c585126ad2f54917a3ad4bb3c
prLorence <paulorencereyes@gmail.com>
2024-07-21 14:53:49 +0800

    feat: go hello world, needed for dockerfile

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// what i will need
+// sqlc
+// fiber
+// middleware, for authorization
+// decoder from request to application struct
+// db connection string comes from os.env, which come from config struct, might need an if statement for dev purposes
+// session manager
+// a seeder
+
+func main() {
+	fmt.Println("hello world")
+}
