48e96320c1fe5ef28c199659374b47a8e451264d
prLorence <paulorencereyes@gmail.com>
2024-07-22 21:44:22 +0800

    feat: simple login and logout

--- internal/users/handler.go
+++ internal/users/handler.go
@@ -2,22 +2,24 @@ package users
 
 import (
 	"context"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/storage/postgres/v3"
 	"github.com/prLorence/books-api/internal/db"
+	"github.com/prLorence/books-api/internal/utils"
 )
 
 // login sets the session of user id
 // logout removes the user id from the session
 type UserDto struct {
-	Username string `json:"user_name"`
+	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-func Login(c *fiber.Ctx, pg *db.Queries, store *session.Store) error {
+func Login(c *fiber.Ctx, pg *db.Queries, store *postgres.Storage) error {
 	req := UserDto{}
-	if err := c.BodyParser(req); err != nil {
+	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
 			"errors": err.Error(),
 		})
@@ -33,7 +35,13 @@ func Login(c *fiber.Ctx, pg *db.Queries, store *session.Store) error {
 		})
 	}
 
-	// sess, err := store.Get(c)
+	store.Set("userId", utils.Int32ToBytes(id), 12*time.Hour)
 
-	return c.JSON(id)
+	return c.JSON(fiber.Map{
+		"userId": id,
+	})
+}
+
+func Logout(c *fiber.Ctx, pg *db.Queries, store *postgres.Storage) error {
+	return store.Delete("userId")
 }
