ead1c96093df4205b5706418085a308b547110cf
prLorence <paulorencereyes@gmail.com>
2024-07-22 21:43:47 +0800

    feat: users routes

--- internal/users/routes.go
+++ internal/users/routes.go
@@ -1,9 +1,16 @@
 package users
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"github.com/prLorence/books-api/internal/utils"
 )
 
 func SetupRoutes(srv *utils.Server) {
+	srv.App.Post("/login", func(c *fiber.Ctx) error {
+		return Login(c, srv.DB, srv.Session)
+	})
 
+	srv.App.Get("/logout", srv.RequireAuth, func(c *fiber.Ctx) error {
+		return Logout(c, srv.DB, srv.Session)
+	})
 }
