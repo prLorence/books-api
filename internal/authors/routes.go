1c7b627d435635b1a8ce65b25097ee3e4b4f39db
prLorence <paulorencereyes@gmail.com>
2024-07-22 21:40:51 +0800

    feat: use auth and authz middleware

--- internal/authors/routes.go
+++ internal/authors/routes.go
@@ -2,33 +2,32 @@ package authors
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/prLorence/books-api/internal/utils"
 )
 
 func SetupRoutes(srv *utils.Server) {
-	srv.App.Post("/authors", func(c *fiber.Ctx) error {
+	srv.App.Post("/authors", srv.RequireAuth, srv.RequireAdmin, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return Create(c, srv.DB)
-	}).Use(adaptor.HTTPMiddleware(srv.RequireAdmin))
+	})
 
-	srv.App.Get("/authors", func(c *fiber.Ctx) error {
+	srv.App.Get("/authors", srv.RequireAuth, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return All(c, srv.DB)
 	})
 
-	srv.App.Get("/authors/:id", func(c *fiber.Ctx) error {
+	srv.App.Get("/authors/:id", srv.RequireAuth, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return GetById(c, srv.DB)
 	})
 
-	srv.App.Put("/authors/:id", func(c *fiber.Ctx) error {
+	srv.App.Put("/authors/:id", srv.RequireAuth, srv.RequireAdmin, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return Update(c, srv.DB)
-	}).Use(adaptor.HTTPMiddleware(srv.RequireAdmin))
+	})
 
-	srv.App.Delete("/authors/:id", func(c *fiber.Ctx) error {
+	srv.App.Delete("/authors/:id", srv.RequireAuth, srv.RequireAdmin, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return RemoveById(c, srv.DB)
-	}).Use(adaptor.HTTPMiddleware(srv.RequireAdmin))
+	})
 }
--- internal/books/routes.go
+++ internal/books/routes.go
@@ -2,38 +2,37 @@ package books
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/prLorence/books-api/internal/utils"
 )
 
 func SetupRoutes(srv *utils.Server) {
-	srv.App.Post("/books", func(c *fiber.Ctx) error {
+	srv.App.Post("/books", srv.RequireAuth, srv.RequireAdmin, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return Create(c, srv.DB)
-	}).Use(adaptor.HTTPMiddleware(srv.RequireAdmin))
+	})
 
 	srv.App.Get("/books", func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return All(c, srv.DB)
 	})
 
-	srv.App.Get("/books/:id", func(c *fiber.Ctx) error {
+	srv.App.Get("/books/:id", srv.RequireAuth, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return GetById(c, srv.DB)
 	})
 
-	srv.App.Get("/books/:title", func(c *fiber.Ctx) error {
+	srv.App.Get("/books/:title", srv.RequireAuth, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return GetByTitle(c, srv.DB)
 	})
 
-	srv.App.Put("/books/:id", func(c *fiber.Ctx) error {
+	srv.App.Put("/books/:id", srv.RequireAuth, srv.RequireAdmin, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return Update(c, srv.DB)
-	}).Use(adaptor.HTTPMiddleware(srv.RequireAdmin))
+	})
 
-	srv.App.Delete("/books/:id", func(c *fiber.Ctx) error {
+	srv.App.Delete("/books/:id", srv.RequireAuth, srv.RequireAdmin, func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		return RemoveById(c, srv.DB)
-	}).Use(adaptor.HTTPMiddleware(srv.RequireAdmin))
+	})
 }
