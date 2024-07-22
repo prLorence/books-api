d826368806a3f2d9879acdf8d1e23007c3c166c5
prLorence <paulorencereyes@gmail.com>
2024-07-22 21:46:04 +0800

    feat: server struct in utils package since it can/will be used throughout the project

--- internal/utils/server.go
+++ internal/utils/server.go
@@ -2,13 +2,12 @@ package utils
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/storage/postgres/v3"
 	"github.com/prLorence/books-api/internal/db"
 )
 
 type Server struct {
 	App     *fiber.App
 	DB      *db.Queries
-	Session *session.Store
-
+	Session *postgres.Storage
 }
