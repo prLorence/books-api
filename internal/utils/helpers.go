7eb8cfeb52a3bf90ef3c0efe467c236e94edc48f
prLorence <paulorencereyes@gmail.com>
2024-07-22 21:45:28 +0800

    feat: helpers

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -1,7 +1,31 @@
 package utils
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net/http"
+)
 
 func IsAuthorized(r *http.Request) bool {
 	return false
+}
+
+func Int32ToBytes(i int32) []byte {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, i)
+	if err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+	return buf.Bytes()
+}
+
+func BytesToInt32(b []byte) int32 {
+	var i int32
+	buf := bytes.NewReader(b)
+	err := binary.Read(buf, binary.LittleEndian, &i)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+	return i
 }
